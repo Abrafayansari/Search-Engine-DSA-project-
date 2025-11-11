bb966616152b8833cd1fdab8d515ea0817458d4b
Abrafayansari <ansarirafay919@gmail.com>
2025-11-11 11:12:19 -0800

    completed the task in which we extract words from the all files provided

--- data_extracting_from_file_module.cpp
+++ data_extracting_from_file_module.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_set>
 #include <string>
-#include <sstream>
-#include <cctype>
 #include <vector>
+#include <cctype>
 using namespace std;
 
-void extract_from_files(vector <string>&f){
-	for(int i=0;i<f.size();i++){
-ifstream file(f[i].c_str());
-if (!file) {
-    cout << "Error opening file!" << endl;
-    
-    return;
-}
-string word;
-while (file >> word) {
-	for(int j=0;j<word.length();j++){
-    	word[j]=tolower(word[j]);
-	}
-    cout << word << " "<<f[i] << endl;
+// C++11 unordered_set for stopwords
+unordered_set<string> stopwords = { "a","an","and","are","as","at","be","but","by",
+                                    "for","if","in","into","is","it","no","not","of",
+                                    "on","or","such","that","the","their","then","there",
+                                    "these","they","this","to","was","will","with" };
+
+// Remove punctuation and convert to lowercase
+string remove_unwanted_characters(const string& s) {
+    string s1 = "";
+    for (int i = 0; i < s.length(); i++) {
+        if ((s[i] >= 'A' && s[i] <= 'Z') || 
+            (s[i] >= 'a' && s[i] <= 'z') || 
+            (s[i] >= '0' && s[i] <= '9')) {
+            s1 += tolower(s[i]);
+        }
+    }
+    return s1;
 }
 
-	}
+// Extract words from files and print non-stopwords with line numbers
+void extract_from_files(vector<string>& files) {
+    for (int i = 0; i < files.size(); i++) {
+        ifstream file(files[i].c_str());
+        if (!file) {
+            cout << "Error opening file: " << files[i] << endl;
+            continue;
+        }
+
+        string line;
+        int lineNumber = 0;
+        while (getline(file, line)) {
+            lineNumber++;
+            string word = "";
+            for (int j = 0; j <= line.length(); j++) {
+                if (j == line.length() || !isalnum(line[j])) {
+                    if (!word.empty()) {
+                        string cleaned = remove_unwanted_characters(word);
+                        if (!cleaned.empty() && stopwords.find(cleaned) == stopwords.end()) {
+                            cout << cleaned << " (" << files[i] << ", line " << lineNumber << ")" << endl;
+                        }
+                        word = "";
+                    }
+                } else {
+                    word += line[j];
+                }
+            }
+        }
+    }
 }
 
-int main(){
-	vector <string>f;
-	f.push_back("t1.txt");
-		f.push_back("t2.txt");
+int main() {
+    vector<string> files;
+    files.push_back("t1.txt");
+    files.push_back("t2.txt");
 
-		extract_from_files(f);
-	
+    extract_from_files(files);
+
+    return 0;
 }
